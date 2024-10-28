7649a9f22a4225e1835f1f0412c50c266c55904c
monu001-og <olatudeogbe@gmail.com>
2024-10-28 06:48:32 +0100

    print.py

---
+++ print.py
@@ -0,0 +1,8 @@
+#printing a greeting message
+print("Hello,World!")
+#printing my name
+print(" Monu")
+#printing a fun fact about myself
+print("I love playing football")
+#performing and printing asimple calculation
+print("THE RESULT OF 3 + 5 IS:",3+5)
